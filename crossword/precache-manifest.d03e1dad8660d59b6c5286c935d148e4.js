aa3f74437f35ed5cad5c0e1b28bdcd29e8c0ce82
patrickweaver <patrickweaver@users.noreply.github.com>
2020-11-03 17:05:53 +0000

    deploy: 08b0605659942c8753a5ca12be4d184aab75ce0b

---
+++ crossword/precache-manifest.d03e1dad8660d59b6c5286c935d148e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbeab94e93f6e7d0997bd7bbab64d2ad",
+    "url": "/crossword/index.html"
+  },
+  {
+    "revision": "6207b9a2fec4f7f7e8df",
+    "url": "/crossword/static/css/main.ddcea324.chunk.css"
+  },
+  {
+    "revision": "fd5865127c446011685c",
+    "url": "/crossword/static/js/2.229e763e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crossword/static/js/2.229e763e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6207b9a2fec4f7f7e8df",
+    "url": "/crossword/static/js/main.f103a12e.chunk.js"
+  },
+  {
+    "revision": "e7212ad0492691aaf7c8",
+    "url": "/crossword/static/js/runtime-main.4c3f6f8d.js"
+  }
+]);
