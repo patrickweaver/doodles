9329e35e1aa6b7aa6f3865deb448ce1a4af43e22
Patrick Weaver <pwdev@weavermail.org>
2026-05-17 13:39:21 -0700

    update parallel map tiles, styles

--- build/drawing-parallel-lines-on-a-map/leaflet/config.js
+++ build/drawing-parallel-lines-on-a-map/leaflet/config.js
@@ -1,6 +1,6 @@
 var MAP_CENTER = [40.688089, -73.966839];
 var MAP_ZOOM = 15;
 var TILE_LAYER =
-  "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png";
+  "https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}";
 var METER_LAT_OFFSET = 0.000008983152841324227;
 var METER_LNG_OFFSET = 0.000011855720423170624;
