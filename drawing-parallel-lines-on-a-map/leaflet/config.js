ee80aa43573faf09f73ea593df0727e48985d66d
patrickweaver <patrickweaver@users.noreply.github.com>
2026-05-17 20:51:07 +0000

    deploy: 259317c4b2c5078f2a032c1e8746668f1c973c8a

--- drawing-parallel-lines-on-a-map/leaflet/config.js
+++ drawing-parallel-lines-on-a-map/leaflet/config.js
@@ -2,5 +2,6 @@ var MAP_CENTER = [40.688089, -73.966839];
 var MAP_ZOOM = 15;
 var TILE_LAYER =
   "https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}";
+var TILE_ATTRIBUTION = `'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'`;
 var METER_LAT_OFFSET = 0.000008983152841324227;
 var METER_LNG_OFFSET = 0.000011855720423170624;
